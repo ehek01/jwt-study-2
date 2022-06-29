fece0d12a81955951c25062f6bc5ecde57facea2
Dodari <whdgus0515@gmail.com>
2022-06-30 03:54:18 +0900

    token 제공완료.(refresh, access)

--- src/main/java/io/getarrays/jwtstudy2/config/SecurityConfig.java
+++ src/main/java/io/getarrays/jwtstudy2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import io.getarrays.jwtstudy2.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
@@ -42,17 +44,31 @@ public class SecurityConfig {
         AuthenticationManager authenticationManager
                 = authenticationManagerBuilder.build();
 
+        // 해당 개체를 사용하여 URL 을 변경할 수 있으며, 사용자를 지정할 수 있는 몇가지 다른 항목도 있습니다.
+        // 그래서 여기로 가서 필터를 설정하고 URL 을 처리할 수 있습니다.
+        /**
+         *  원래는 UsernamePasswordAuthenticationFilter 에서 /login 이 기본으로 구현되어 있지만,
+         *  다른 주소로 해주고 싶으면 이런방식을 사용할 수 있습니다.
+         */
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(STATELESS) // 세션 설정 끄기
 
                 // 권한 설정
                 .and()
-                .authorizeHttpRequests().anyRequest().permitAll()
+                .authorizeHttpRequests()
+                .antMatchers("/api/login/**").permitAll()
+                .antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER")
+                .antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN") // 사용자를 저장하기 위해서는 관리자 권한이 필요.
+                .anyRequest().authenticated() // 모든경로는 인증을 받아야 한다.
 
+                // 필터랑 password encoder 등 이것저것 추가?
                 .and()
                 .authenticationManager(authenticationManager)
-                .addFilter(new CustomAuthenticationFilter(authenticationManager));
+                .addFilter(customAuthenticationFilter);
 
         return http.build();
     }
--- src/main/java/io/getarrays/jwtstudy2/filter/CustomAuthenticationFilter.java
+++ src/main/java/io/getarrays/jwtstudy2/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package io.getarrays.jwtstudy2.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,7 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 // 로그인이 성공할 때마다 access token 과 refresh token 을 발급해줄 filter
 @Slf4j
@@ -66,7 +71,16 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 
-        response.setHeader("access_token", access_token);
-        response.setHeader("refresh_token", refresh_token);
+        /* token header 로 던지기 */
+//        response.setHeader("access_token", access_token);
+//        response.setHeader("refresh_token", refresh_token);
+
+        /* token body 로 던지기 */
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+
+        response.setContentType("application/json");
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
